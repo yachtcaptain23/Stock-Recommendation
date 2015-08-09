9c8c11516773462ada45c09412f1f163a7ef2f5a
yachtcaptain23 <amwang217@gmail.com>
2015-08-09 12:08:12 -0700

    Cleaned up imports. Provided default tracked symbols

--- stock_rec_aggr.py
+++ stock_rec_aggr.py
@@ -1,6 +1,18 @@
-import urllib2
-import re
+#!/usr/bin/python
 
+import urllib2
+import re, argparse
+
+
+def readTickersFromText(textfile):
+  f_open = open(textfile,'r',)
+  lines = f_open.readlines()
+  f_open.close()
+
+  for line in lines:
+    val = grab_site(line[:-1])
+    if(val):
+      print val
 
 def grab_site(post_text):
   url = "http://finance.yahoo.com/q?s=" + post_text
@@ -9,6 +21,8 @@ def grab_site(post_text):
   lines = f.read()
   counter = 0
   line = [""]
+  rec_mo = None
+  eps_mo = None
   for char in lines:
     if char == '\n':
       counter+=1
@@ -21,20 +35,29 @@ def grab_site(post_text):
   lines = line
   
   for line in lines:
-    mo = re.search("Mean Recommendation.*?\>(\d+(\.\d{1,2})?)",line,)
-    if mo:
-      return "%s%s"%(post_text, mo.group(1))
+    if not rec_mo:
+      rec_mo = re.search("Mean Recommendation.*?\>(\d+(\.\d{1,2})?)",line,)
+    if not eps_mo:
+      # eps_mo = re.search("Annual EPS Est.*?\>((|-)\d+(\.\d{1,2})?)",line)
+      eps_mo = re.search("Annual EPS Est(.*)",line)
+      # First table value is for Annuel EPS Est from Yahoo website June 2014
+      # </small>:</th><td class="yfnc_tabledata1">0.83</td></tr><tr><th scope="row" width="50%">Quarterly EPS Est<small>
 
-#
-#url = "http://finance.yahoo.com/q?s=FB"
+      eps_mo = re.search("yfnc_tabledata1\">(\d+(\.\d{1,2})?)</td></tr><tr><th scope=\"row\" width=\"50%\">Quarterly",line,);
+    if rec_mo and eps_mo:
+      return "%s rec=%s eps=%s"%(post_text, rec_mo.group(1), eps_mo.group(1))
 
-#f = urllib2.urlopen(url)
-#print f.read()
+'''
+Argument parsing
+'''
+def parse_arguments():
+  parser = argparse.ArgumentParser(description='')
+  parser.add_argument('-filename',type=str,nargs=1,help='')
+  args = parser.parse_args()
+  return args.filename[0]
 
-print grab_site("fb")
-print grab_site("yhoo")
-print grab_site("msft")
-print grab_site("intc")
-print grab_site("ddd")
-print grab_site("amzn")
+def __main__():
+  filename = parse_arguments()
+  readTickersFromText(filename)
 
+__main__()
